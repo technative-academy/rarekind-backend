488e0970b55f88aa4da04a1d8a4175041c6d275f
yodiyo <yodiyo@users.noreply.github.com>
2025-11-06 17:12:20 +0000

    Prettified Code!

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ import usersRoutes from './src/routes/users.js'
 import animalsRoutes from './src/routes/animals.js'
 import collectionsRoutes from './src/routes/collections.js'
 
-
 const app = express()
 const PORT = process.env.PORT || 3000
 
