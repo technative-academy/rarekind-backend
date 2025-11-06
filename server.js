4268dca2265e65213fe9504937f102d8d60dded3
YashMagane <yash.magane.11@gmail.com>
2025-11-06 14:15:08 +0000

    collections json and routes working

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import usersRoutes from "./src/routes/users.js";
+import collectionsRoutes from "./src/routes/collections.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/users", usersRoutes);
+app.use("/collections", collectionsRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
