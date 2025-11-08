04943e67aa4f703c64012563705f7aaafd3f371a
YashMagane <yash.magane.11@gmail.com>
2025-11-08 12:58:59 +0000

    species json and animals link done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import usersRoutes from './src/routes/users.js'
 import collectionsRoutes from './src/routes/collections.js'
+import speciesRoutes from './src/routes/species.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -8,5 +9,6 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use('/users', usersRoutes)
 app.use('/collections', collectionsRoutes)
+app.use('/species', speciesRoutes)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
