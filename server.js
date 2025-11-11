9c45230bbcdb0649e8cd6ead5dcd741235516c02
YashMagane <yash.magane.11@gmail.com>
2025-11-11 23:11:57 +0000

    logging in and registering with auth

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import usersRoutes from './src/routes/users.js'
 import animalsRoutes from './src/routes/animals.js'
 import collectionsRoutes from './src/routes/collections.js'
 import speciesRoutes from './src/routes/species.js'
+import authRouter from './src/routes/auth.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -12,5 +16,6 @@ app.use('/users', usersRoutes)
 app.use('/animals', animalsRoutes)
 app.use('/collections', collectionsRoutes)
 app.use('/species', speciesRoutes)
+app.use('/auth', authRouter)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
