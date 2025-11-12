329255fdfcc21779209b4b277e099944ade2e81c
YashMagane <yash.magane.11@gmail.com>
2025-11-12 09:49:01 +0000

    trying to fix CORS error

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+})
+
 app.use(express.json())
 app.use('/users', usersRoutes)
 app.use('/animals', animalsRoutes)
