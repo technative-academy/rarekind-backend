bc0c9353cf5bd968c2d662dd49a7536936f6d99b
yodiyo <yodiyo@users.noreply.github.com>
2025-11-20 17:29:06 +0000

    Prettified Code!

--- src/models/animalsModel.js
+++ src/models/animalsModel.js
@@ -15,8 +15,9 @@ export const getAllAnimals = async () => {
 }
 
 export const getAnimalById = async (id) => {
-    const [rows] = await pool.query('SELECT  a.id, a.collection_id, a.name, a.classification_id, c.name AS classification_name, a.description, a.image_url FROM animals AS a JOIN classifications AS c ON a.classification_id = c.id WHERE a.id = ?', [id]
-
+    const [rows] = await pool.query(
+        'SELECT  a.id, a.collection_id, a.name, a.classification_id, c.name AS classification_name, a.description, a.image_url FROM animals AS a JOIN classifications AS c ON a.classification_id = c.id WHERE a.id = ?',
+        [id]
     )
     return rows[0]
 }
@@ -36,10 +37,12 @@ export const createAnimal = async ({
     return {
         id: result.insertId,
         collection_id,
-        name, description, classification_id, image_url
+        name,
+        description,
+        classification_id,
+        image_url,
     }
 }
-
 
 export const updateAnimal = async ({
     id,
@@ -56,12 +59,7 @@ export const updateAnimal = async ({
     return { id, collection_id, name, description, classification_id, image_url }
 }
 
-export const deleteAnimal = async ({
-    id,
-}) => {
-    const [result] = await pool.query(
-        'DELETE FROM animals WHERE id = ?',
-        [id]
-    )
+export const deleteAnimal = async ({ id }) => {
+    const [result] = await pool.query('DELETE FROM animals WHERE id = ?', [id])
     return { id }
 }
